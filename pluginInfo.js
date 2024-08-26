7775f600adcff6810fd6ef26597377d3895e7143
marlonalbto18 <125631832+marlonalbto18@users.noreply.github.com>
2024-08-25 21:07:42 -0700

    Create pluginInfo.js
    S

---
+++ pluginInfo.js
@@ -0,0 +1,19 @@
+{
+    "name": "GlobalBadges",
+    "version": "0.1",
+    "description": "Adds global badges from other client mods",
+    "authors": [
+        {
+            "name": "marrrrrrrrrrrrrrrrrrrrrrrrrrrr",
+            "id": "1246661163354296481"
+        }
+    ],
+    "color": "#E54B4B",
+    "rawUrl": "https://raw.githubusercontent.com/domi-btnr/Enmity-Stuff/main/dist/GlobalBadges.js",
+    "sourceUrl": "https://github.com/domi-btnr/Enmity-Stuff/tree/main/GlobalBadges",
+    "changelog": [
+        "Fixed Badges not appearing on Profiles",
+        "Fixed Settings repository button not opening and updated descriptions."
+    ],
+    "hash": "DEBUG"
+}
